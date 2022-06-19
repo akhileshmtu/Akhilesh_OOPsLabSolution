c78a9603e8ee07e342b534923f8a0d17fe51eb4c
akhileshmtu <96040228+akhileshmtu@users.noreply.github.com>
2022-06-19 20:38:32 +0530

    Add files via upload

---
+++ src/com/OOPsLabSolution/Service/DriverClass.java
@@ -0,0 +1,56 @@
+package com.OOPsLabSolution.Service;
+
+import java.util.Scanner;
+
+import com.OOPsLabSolution.model.Employee;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		Employee employee1 = new Employee("harshit","choudary");
+		String generatedEmail;
+		char[] generatedPassword;
+		CredentialService cService= new CredentialService();
+		Scanner scan =new Scanner(System.in);
+		int choice;
+		
+		
+		
+		System.out.println("Please enter the department from the following");
+		System.out.println("1. Technical Dept.");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal Dept.");
+		
+		choice=scan.nextInt();
+		if(choice==1) {
+			generatedEmail = cService.generateEmailAddress(employee1.getFirstName(),employee1.getLastName(),"tech");
+		generatedPassword= cService.generatePassword();
+		cService.showCredentials(employee1, generatedEmail, generatedPassword);
+			}
+		
+		else if(choice==2) {
+			generatedEmail = cService.generateEmailAddress(employee1.getFirstName(),employee1.getLastName(),"admin");
+			generatedPassword= cService.generatePassword();
+			cService.showCredentials(employee1, generatedEmail, generatedPassword);
+		}
+		else if(choice==3) {
+			generatedEmail = cService.generateEmailAddress(employee1.getFirstName(),employee1.getLastName(),"HR");
+			generatedPassword= cService.generatePassword();
+			cService.showCredentials(employee1, generatedEmail, generatedPassword);
+		}
+		else if(choice==4) {
+			generatedEmail = cService.generateEmailAddress(employee1.getFirstName(),employee1.getLastName(),"legal");
+			generatedPassword= cService.generatePassword();
+			cService.showCredentials(employee1, generatedEmail, generatedPassword);
+		}
+		else 
+			System.out.println("enter a valid choice");
+			
+	
+		
+	
+
+	}
+
+}
